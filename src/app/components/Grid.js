446561b0f91e27bb88be0ac4840942c32a22e0cb
tabeelanwar <tabeelanwar21@gmail.com>
2025-10-14 21:21:04 +0500

    marriage-app

--- src/app/components/Grid.js
+++ src/app/components/Grid.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function Grid() {
-  return (
-    <div className='px-4'>
-      <div className='grid gap-4 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-        <div className='border'>sd</div>
-        <div className='border'>sd</div>
-        <div className='border'>sd</div>
-      </div>
-    </div>
-  )
-}
+// function Grid() {
+//   return (
+//     <div className='px-4'>
+//       <div className='grid gap-4 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+//         <div className='border'>sd</div>
+//         <div className='border'>sd</div>
+//         <div className='border'>sd</div>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Grid
+// export default Grid
